eda0b7383f38399e01c05a056b958ab38caa14cb
kangyun9957 <rlarkddbs3@naver.com>
2022-01-20 16:33:58 +0900

    change

---
+++ Readme.md
@@ -0,0 +1,15 @@
+#Youtube Reloaded
+
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/delete -> Delete Video
+/videos/comments -> Comment Video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import express from "express";
+import morgan from "morgan";
+import res from "express/lib/response";
 const app=express();
+const logger=morgan("dev");
 const PORT=4000;
+
+const handlehome = (req,res) => {
+    return res.send("sex");
+};
+
+const handlelogin = (req,res) => {
+    return res.send("login");
+}; 
+app.use(logger);
+app.get("/",handlehome);
+app.get("/login",handlelogin);
 const handleListening = () => console.log(`Server hosting http://localhost:${PORT}`);
 app.listen(PORT,handleListening)
